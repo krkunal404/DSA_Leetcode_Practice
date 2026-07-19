50a2314a4bd5f49f6ef4eed81975571df3756f38
Kunnal <krkunal404@gmail.com>
2026-07-19 19:25:52 +0530

    Time: 440 ms (58.22%), Space: 0B (100%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+) AS unique_numbers;
+
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
