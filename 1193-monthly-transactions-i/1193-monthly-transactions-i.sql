aecceaa52271146310ba91347f55e93127ccb385
Kunnal <krkunal404@gmail.com>
2026-07-28 23:01:06 +0530

    Time: 2010 ms (5.01%), Space: 0B (100%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,16 @@
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    SUM(state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE
+            WHEN state = 'approved' THEN amount
+            ELSE 0
+        END) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
